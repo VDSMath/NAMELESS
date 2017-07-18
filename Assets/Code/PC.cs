57cdd4e5da154c48d361a035f289debbe8b06877
VDSMath <matheusvdsdf@gmail.com>
2017-07-18 18:21:31 -0300

    Personagem anda, pode atacar de 2 maneiras e inimigo básico

---
+++ Assets/Code/PC.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    struct Direction
+    {
+        public bool zero;
+        public Vector2 dir;
+        public string d;
+
+        public Direction(bool z, Vector2 di, string dee)
+        {
+            this.zero = z;
+            this.dir = di;
+            d = dee;
+        }
+
+    }
+
+    public float dashCooldown, 
+                 dashSpeed,
+                 moveSpeed;
+    private bool dashed;
+
+    public int attackMode; //0 = Shoot, 1 = Slash
+
+    private Vector2 shotDirection;
+    public GameObject bullet;
+    public float bulletCooldown,
+                 bulletDamage,
+                 bulletDestroyTime,
+                 bulletSpeed,
+                 playerEffect,
+                 swordCooldown,
+                 swordDamage;
+    private float bulletTime,
+                  swordTime;
+
+    public GameObject sword;
+
+    public AudioSource pew;
+
+    // Use this for initialization
+    void Start()
+    {
+        attackMode = 0;
+        swordTime = 0;
+        bulletTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        AttackMode();
+        Attack();
+    }
+
+    private void AttackMode()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            attackMode = (attackMode + 1) % 2;
+        }
+    }
+
+    private void Attack()
+    {
+        swordTime -= Time.deltaTime;
+        bulletTime -= Time.deltaTime;
+        Debug.Log(bulletTime);
+
+        switch (attackMode)
+        {
+            case 0:
+                if (bulletTime <= 0)
+                {
+                    Shoot();
+                }
+                break;
+
+            case 1:
+                if (swordTime <= 0)
+                {
+                    StartCoroutine(Slash());
+                }
+                break;
+        }
+    }
+
+	private IEnumerator Slash() {
+		Direction d = new Direction(true, Vector2.zero, "");
+		d = AttackDirection(d);
+
+		if (!d.zero) {
+
+			switch (d.d) {
+				case ("up"):
+					transform.rotation = Quaternion.Euler(Vector3.zero);
+					break;
+				case ("down"):
+					transform.rotation = Quaternion.Euler(0, 0, 180);
+					break;
+				case ("right"):
+					transform.rotation = Quaternion.Euler(0, 0, 270);
+					break;
+				case ("left"):
+					transform.rotation = Quaternion.Euler(0, 0, 90);
+					break;
+			}
+
+            swordTime = swordCooldown;
+            sword.SetActive(true);
+			yield return new WaitForSeconds(.3f);
+			sword.SetActive(false);
+		}
+		
+    }
+
+    private void Shoot()
+    {
+        Direction direction = new Direction(true, Vector2.zero,"");
+        direction = AttackDirection(direction);
+
+        if (!direction.zero)
+        {
+            ShotRelease(direction);
+        }
+    }
+
+    private Direction AttackDirection(Direction direction)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction.dir.y = 1;
+            direction.zero = false;
+            direction.d = "up";
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction.dir.y = -1;
+            direction.zero = false;
+            direction.d = "down";
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction.dir.x = 1;
+            direction.zero = false;
+            direction.d = "right";
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction.dir.x = -1;
+            direction.zero = false;
+            direction.d = "left";
+        }
+
+        return direction;
+    }
+
+    private void ShotRelease(Direction direction)
+    {
+        pew.Play();
+        bulletTime = bulletCooldown;
+        GameObject b = Instantiate(bullet);
+		GameObject.Destroy(b, bulletDestroyTime);
+        b.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        b.transform.position = this.transform.position;
+        Vector2 temp = new Vector2(bulletSpeed * direction.dir.x, bulletSpeed * direction.dir.y);
+        Vector2 vel = this.GetComponent<Rigidbody2D>().velocity * playerEffect;
+        b.GetComponent<Rigidbody2D>().AddForce(temp);
+        b.GetComponent<Rigidbody2D>().velocity += vel;
+    }
+
+    private void Move()
+    {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * moveSpeed,
+                                                              Input.GetAxis("Vertical") * moveSpeed));
+
+        if (Input.GetKeyDown(KeyCode.Space) && !dashed)
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxisRaw("Horizontal"),
+                                                              Input.GetAxisRaw("Vertical")) * dashSpeed);
+            StartCoroutine(DashCooldown());
+        }
+    }
+
+    private IEnumerator DashCooldown()
+    {
+        dashed = true;
+        yield return new WaitForSeconds(dashCooldown);
+        dashed = false;
+    }
+}
---
+++ Assets/Code/SimpleEnemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour {
+
+    public float moveMaxSpeed;
+
+    public float knockbackDistance,
+                 maxHP;
+    private float currentHP;
+
+    public GameObject target;
+    public AudioSource oh;
+
+	// Use this for initialization
+	void Start () {
+        currentHP = maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FollowTarget();
+	}
+    void FollowTarget()
+    {
+        LookAt2D();      
+	    transform.eulerAngles = new Vector3(0,0, transform.eulerAngles.z);
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveMaxSpeed);
+    }
+
+    private void LookAt2D()
+    {
+        Vector3 diff = target.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            GameObject.Destroy(collision.gameObject);
+            TakeDamage(target.GetComponent<PC>().bulletDamage, knockbackDistance * 0.1f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Sword")
+        {
+            TakeDamage(target.GetComponent<PC>().swordDamage, knockbackDistance * 10f	);
+        }
+    }
+
+    private void TakeDamage(float amount, float knockback)
+    {
+        Vector2 knockbackV = -transform.up.normalized;
+        Debug.Log(knockbackV);
+        GetComponent<Rigidbody2D>().AddForce(knockbackV * knockback);
+        currentHP -= amount;
+
+        if(currentHP <= 0)
+        {
+            Kill();
+        }
+    }
+
+    private void Kill()
+    {
+
+        oh.Play();
+        gameObject.SetActive(false);
+    }
+}
