79e639308f839ae447a9d971d5edb167db6b3bb8
VDSMath <matheusvdsdf@gmail.com>
2017-09-09 10:24:40 -0300

    Começo do Overworld

---
+++ Assets/Code/Level/Overworld/CameraHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour {
+
+    private GameObject mainCam, player;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = Camera.main.gameObject;
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FollowPlayer();
+	}
+
+    void FollowPlayer()
+    {
+        mainCam.transform.position = new Vector3(player.transform.position.x, 
+                                                 player.transform.position.y, 
+                                                 mainCam.transform.position.z);
+    }
+}
